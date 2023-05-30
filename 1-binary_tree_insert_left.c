30e86dbbf5e17b49bb3764e9c77f03498fae0e0f
LabyKreative <labykreative@yahoo.com>
2023-05-30 06:17:46 -0700

    Insert Left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -27,7 +27,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if ((*parent).left)
 	{
 		(*new).left = (*parent).left;
-		(*parent).(*left).parent = new;
+		(*parent).left->parent = new;
 	}
 	(*parent).left = new;
 	return (new);
