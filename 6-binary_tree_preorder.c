1ebcb032fe959cd6593b5dbc2069a94e6863eae8
LabyKreative <labykreative@yahoo.com>
2023-05-30 15:11:44 -0700

    Pre-order traversal!

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - a function that goes through a binary tree using pre-order traversal
+ * binary_tree_preorder - a function that goes through a
+ * binary tree using pre-order traversal
  * @tree: is a pointer to the root node of the tree to traverse
  * @func: is a pointer to a function to call for each node
  *
