d18e29926b3c7ed23d78dac8d510381fe9534500
LabyKreative <labykreative@yahoo.com>
2023-06-01 02:27:55 -0700

    Heap - Extract

---
+++ 133-heap_extract.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * heap_to_sorted_array - a function that extracts the
+ * root node of a Max Binary Heap
+ *
+ * @root: is a double pointer to the root node of heap
+ * Return: value stored in the root node
+ */
+int heap_extract(heap_t **root)
+{
+	return (root);
+}
