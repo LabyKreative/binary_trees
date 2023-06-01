62b08861c4b896872729408f00f4763464229136
LabyKreative <labykreative@yahoo.com>
2023-05-31 21:17:37 -0700

    Level-Order Traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,20 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height_aux -  a tree
- * @tree: Pointer to
- * Return: Trees
+ * binary_tree_height_aux -  a tree function
+ * @tree: pointer to tree
+ * Return: trees
  */
-size_t	binary_tree_height_aux(const binary_tree_t *tree)
+size_t binary_tree_height_aux(const binary_tree_t *tree)
 {
 	size_t hleft = 0, hright = 0;
 
 	if (!tree)
 		return (0);
-	if (tree->left)
-		hleft = 1 + binary_tree_height_aux(tree->left);
-	if (tree->right)
-		hright = 1 + binary_tree_height_aux(tree->right);
+	if ((*tree).left)
+		hleft = 1 + binary_tree_height_aux((*tree).left);
+	if ((*tree).right)
+		hright = 1 + binary_tree_height_aux((*tree).right);
 
 	if (hleft > hright)
 		return (hleft);
@@ -22,11 +22,11 @@ size_t	binary_tree_height_aux(const binary_tree_t *tree)
 }
 
 /**
- * print_level_order - print each
- * @tree: pointer to thse
+ * print_level_order - a function to print each
+ * @tree: pointer to tree
  * @level: level of the tree
  * @func: pointer to a  node
- * Return: void
+ * Return: none
  */
 void print_level_order(const binary_tree_t *tree, int level, void (*func)(int))
 {
@@ -34,20 +34,20 @@ void print_level_order(const binary_tree_t *tree, int level, void (*func)(int))
 		return;
 
 	if (level == 1)
-		func(tree->n);
+		func((*tree).n);
 	else if (level > 1)
 	{
-		print_level_order(tree->left, level - 1, func);
-		print_level_order(tree->right, level - 1, func);
+		print_level_order((*tree).left, level - 1, func);
+		print_level_order((*tree).right, level - 1, func);
 	}
 }
 
 /**
  * binary_tree_levelorder - function that goes through a
  * binary tree using level-order traversal
- * @tree: pointer to the root node of the tree to traverse
- * @func: pointer to a function to call for each node
- * Return: void
+ * @tree: is a pointer to the root node of the tree to traverse
+ * @func: is a pointer to a function to call for each node
+ * Return: none
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
