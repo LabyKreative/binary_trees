e85ae09ea270ae7135e9276fea94ca44a339cb25
LabyKreative <labykreative@yahoo.com>
2023-06-01 02:31:12 -0700

    Heap - Sort

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * heap_to_sorted_array - a function that converts a Binary Max Heap
+ * to a sorted array of integers
+ *
+ * @heap: is a pointer to the root node of the heap to convert
+ * @size: is an address to store the size of the array
+ * Return: array sorted in descending order
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	return (heap);
+}
