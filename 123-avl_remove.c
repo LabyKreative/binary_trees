ad714c2e19b9aab2581aaa4d43d5f0ea442b4665
LabyKreative <labykreative@yahoo.com>
2023-06-01 01:51:38 -0700

    AVL - Remove

---
+++ 123-avl_remove.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * avl_remove - a function that removes a node from an AVL tree
+ *
+ * @root: is a pointer to the root node of the tree for removing a node
+ * @value:  is the value to remove in the tree
+ * Return: pointer to the new root node of the tree after removing
+ * the desired value, and after rebalancing
+ */
+avl_t *avl_remove(avl_t *root, int value)
+{
+	return (root);
+}
