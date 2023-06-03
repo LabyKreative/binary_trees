bc52e014b52c535f858b198094b96bd425a7363b
LabyKreative <labykreative@yahoo.com>
2023-06-03 12:25:33 -0700

    AVL - Remove...

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -1,6 +1,128 @@
 #include "binary_trees.h"
 
 /**
+ * bal - a function that measures balance factor of an AVL
+ * @tree: pointer to check
+ *
+ * Return: bal
+ */
+void bal(avl_t **tree)
+{
+	int balfac;
+
+	if (tree == NULL || *tree == NULL)
+		return;
+	if ((*tree)->left == NULL && (*tree)->right == NULL)
+		return;
+	bal(&(*tree)->left);
+	bal(&(*tree)->right);
+	balfac = binary_tree_balance((const binary_tree_t *)*tree);
+	if (balfac > 1)
+		*tree = binary_tree_rotate_right((binary_tree_t *)*tree);
+	else if (balfac < -1)
+		*tree = binary_tree_rotate_left((binary_tree_t *)*tree);
+}
+/**
+ * successor - a function that gets the next successor
+ * @node: tree to check
+ *
+ * Return: min value
+ */
+int successor(bst_t *node)
+{
+	int left = 0;
+
+	if (node == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		left = successor((*node).left);
+		if (left == 0)
+		{
+			return ((*node).n);
+		}
+		return (left);
+	}
+
+}
+/**
+ *remove_type - a function that removes a node depending of its children
+ *@root: node to remove
+
+ *Return: 0 if it has no children or other value if it has
+ */
+int remove_type(bst_t *root)
+{
+	int new_value = 0;
+
+	if (!(*root).left && !(*root).right)
+	{
+		if ((*root).parent->right == root)
+			(*root).parent->right = NULL;
+		else
+			(*root).parent->left = NULL;
+		free(root);
+		return (0);
+	}
+	else if ((!(*root).left && (*root).right) || (!(*root).right && (*root).left))
+	{
+		if (!(*root).left)
+		{
+			if ((*root).parent->right == root)
+				(*root).parent->right = (*root).right;
+			else
+				(*root).parent->left = (*root).right;
+			(*root).right->parent = (*root).parent;
+		}
+		if (!(*root).right)
+		{
+			if ((*root).parent->right == root)
+				(*root).parent->right = root->left;
+			else
+				(*root).parent->left = (*root).left;
+			(*root).left->parent = (*root).parent;
+		}
+		free(root);
+		return (0);
+	}
+	else
+	{
+		new_value = successor((*root).right);
+		(*root).n = new_value;
+		return (new_value);
+	}
+}
+/**
+ * bst_remove - a function that removes a node from a BST tree
+ * @root: root of the tree
+ * @value: node value to remove
+ *
+ * Return: the tree value
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	int type = 0;
+
+	if (root == NULL)
+		return (NULL);
+	if (value < (*root).n)
+		bst_remove((*root).left, value);
+	else if (value > (*root).n)
+		bst_remove((*root).right, value);
+	else if (value == (*root).n)
+	{
+		type = remove_type(root);
+		if (type != 0)
+			bst_remove((*root).right, type);
+	}
+	else
+		return (NULL);
+	return (root);
+}
+
+/**
  * avl_remove - a function that removes a node from an AVL tree
  *
  * @root: is a pointer to the root node of the tree
@@ -11,5 +133,10 @@
  */
 avl_t *avl_remove(avl_t *root, int value)
 {
-	return (root);
+	avl_t *root_a = (avl_t *) bst_remove((bst_t *) root, value);
+
+	if (root_a == NULL)
+		return (NULL);
+	bal(&root_a);
+	return (root_a);
 }
